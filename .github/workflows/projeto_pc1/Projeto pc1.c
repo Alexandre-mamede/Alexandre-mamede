9d0d9b82530adc9d6b0175808352533fce3db8ed
Alexandre-mamede <133179035+Alexandre-mamede@users.noreply.github.com>
2026-05-29 11:30:19 -0300

    Update Projeto pc1.c

--- .github/workflows/projeto_pc1/Projeto pc1.c
+++ .github/workflows/projeto_pc1/Projeto pc1.c
@@ -2,28 +2,7 @@
 #include <string.h>
 #include <time.h>
 
-typedef struct {
-   char nome[101];
-   char codigoBarras[13];
-   int tipo_produto;
-   int perecivel;
-   char data_Fabricacao[11];
-   char data_Validade[11];
-   float volume;
-   int quantidade_itens;
-   float valorUnitario;
-} Produto;
 
-typedef struct {
-   char nome[51];
-   char cep[10];
-   char endereco[51];
-   char telefone[20];
-   float capacidade_Maxima;
-   float estoque_Atual;
-   Produto produtos[100];
-   int total_Produtos;
-} Deposito;
 
 int main(){
 
