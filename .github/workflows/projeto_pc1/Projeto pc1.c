c518eff58bbd346a9ac1d3782ff86cc3de68123b
Alexandre Mamede Patrocinio <133179035+Alexandre-mamede@users.noreply.github.com>
2026-06-01 16:58:03 -0300

    Update Projeto pc1.c

--- .github/workflows/projeto_pc1/Projeto pc1.c
+++ .github/workflows/projeto_pc1/Projeto pc1.c
@@ -1,12 +1,35 @@
 #include <stdio.h>
 #include <string.h>
 #include <time.h>
-
-
-
+#include <struct.c>
 int main(){
+   
+int select;
 
-   printf("menu")
+    printf("Menu Inicial\n");
+    printf("1. Deposito\n");
+    printf("2. produtos\n");
+    printf("3. salvar e sair\n");
+    
+    scanf("%d",&select);
+    
+    switch (select){
+        
+        case 1:
+            printf("Menu Depósitos");
+            printf("1. Cadastrar novo depósito\n");
+            printf("2. Consultar informações sobre um depósito\n");
+            printf("3. Consultar informações sobre todos os depósitos\n");
+            printf("4. Atualizar dados de um depósito\n");
+            printf("5. Remover um depósito\n");
+            printf("6. Voltar ao menu principal\n");
+            break;
+        case 2:
+        
+        
+    }
+    return 0;
+}
 
 
 }
