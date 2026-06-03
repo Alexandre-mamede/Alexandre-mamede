7575c6b8a816cd5eaa4b03d72560cd785fa293e0
Alexandre Mamede Patrocinio <133179035+Alexandre-mamede@users.noreply.github.com>
2026-06-03 19:59:08 -0300

    Refactor product management and save functionality

--- .github/workflows/projeto_pc1/Projeto pc1.c
+++ .github/workflows/projeto_pc1/Projeto pc1.c
@@ -1,23 +1,44 @@
 #include <stdio.h>
 #include <string.h>
 #include <time.h>
-#include <struct.c>
+#include <struct.h>
+
+void salvar_produto(produto produtos[], int total){
+    
+    FILE *fp = fopen("produtos_salvos.txt","w");
+    
+    if (fp == NULL) {
+        printf("Erro ao abrir o arquivo para salvar.\n");
+    return;
+    }
+    
+    for(int i = 0; i < total; i++){
+        fprintf(fp,"%d;%d;%.2f\n",
+                produtos[i].nome,
+                produtos[i].quantidade_itens,
+                produtos[i].valor_Unitario);
+    }
+    
+    fclose(fp);
+    printf("Arquivo salvo com sucesso!!!\n");
+}
 
 int main(){
-   
-int select;
 
-    printf("Menu Inicial\n");
+produto produtos[100];
+int total = 0;
+int opcao;
+
+    printf("\n=== MENU ===\n\n");
     printf("1. Deposito\n");
-    printf("2. produtos\n");
+    printf("2. Produtos\n");
     printf("3. salvar e sair\n");
+    scanf("%d",&opcao);
     
-    scanf("%d",&select);
-    
-    switch (select){
+    switch (opcao){
         
         case 1:
-            printf("Menu Depósitos");
+            printf("\n===Menu Depósitos===\n\n");
             printf("1. Cadastrar novo depósito\n");
             printf("2. Consultar informações sobre um depósito\n");
             printf("3. Consultar informações sobre todos os depósitos\n");
@@ -25,19 +46,20 @@ int select;
             printf("5. Remover um depósito\n");
             printf("6. Voltar ao menu principal\n");
             break;
+           
         case 2:
-            printf("Menu Deposito");
-            printf("1. Nome do Depósito");
-            printf("2. CEP");
-            printf("3. Endereço");
-            printf("4. Telefone");
-            printf("5. Capacidade máxima");
-            printf("6. Voltar ao menu Depósito");
+            printf("\n===Menu Produtos===\n\n");
+            printf("1. Cadastrar novo produto\n");
+            printf("2. Consultar produto\n");
+            printf("3. Atualizar dados de um produto\n");
+            printf("4. Remover produto\n");
+            printf("5. Transferir produto para outro depósito\n");
+            printf("6. Voltar ao menu principal\n");
             break;
        case 3:
+            salvar_produto(produtos,total);
+            printf("saindo......");
+            break;
     }
     return 0;
-}
-
-
 }
